488ae3daa9725926a909c680112465e3fa5080d0
Ramesh Pradhan <pradhanmesh88@gmail.com>
2025-08-15 01:12:42 +1000

    Commit before fixing line endings

--- rp-blog/terraform-nextjs/main.tf
+++ rp-blog/terraform-nextjs/main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 # S3 Bucket
 resource "aws_s3_bucket" "nextjs_bucket" {
   bucket = "nextjs-portfolio-bucket-rpra"
+   
 }
 # Ownership Control
 resource "aws_s3_bucket_ownership_controls" "nextjs_bucket_ownership_controls" {
@@ -47,4 +48,47 @@ resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
       }
     ]
   })
+}
+
+# Origin Access Identity 
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "OAI for Next.JS portfolio site"
+}
+# Cloudfront distribution 
+resource "aws_cloudfront_distribution" "nextjs_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.nextjs_bucket.bucket_regional_domain_name
+    origin_id = "S3-nextjs-portfolio-bucket"
+    s3_origin_config {
+     origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+
+    }
+  }
+  enabled = true
+  is_ipv6_enabled = true
+  comment = "Next.js portfolio site"
+  default_root_object = "index.html"
+  default_cache_behavior {
+    allowed_methods = [ "GET", "HEAD", "OPTIONS" ]
+    cached_methods = ["GET","HEAD"]
+    target_origin_id = "S3-nextjs-portfolio-bucket"
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl = 0
+    default_ttl = 3600
+    max_ttl = 86400
+  }
+    restrictions {
+      geo_restriction {
+        restriction_type = "none"
+      }
+    }
+    viewer_certificate {
+      cloudfront_default_certificate = true
+    }
 }
--- rp-blog/terraform-nextjs/state.tf
+++ rp-blog/terraform-nextjs/state.tf
@@ -1,8 +1,8 @@
 terraform {
     backend "s3" {
-       bucket = "rp-my-tf-website-state"
+       bucket = "rpra-my-tf-website-state"
        key = "global/s3/terraform.tfstate"
        region = "ap-southeast-2"
-       dynamodb_table = "terraform-lock-file " 
+       dynamodb_table = "my-db-rpra-website-table" 
     }
 }
